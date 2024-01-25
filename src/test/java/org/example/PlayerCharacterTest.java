7cdf8b2941c64a9b87ba8dd677ca483bf62484f7
martinhermes <okapiposter@gmail.com>
2024-01-25 11:56:37 +0100

    added getY Test and Classmethod

--- src/test/java/org/example/PlayerCharacterTest.java
+++ src/test/java/org/example/PlayerCharacterTest.java
@@ -7,11 +7,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PlayerCharacterTest {
 
     @Test
-    void getX_Return_zeroAsValue(){
+    void getX_Return_zeroAsValue() {
         String given = "inputfield_0";
 
-
         int actual = PlayerCharacter.getX();
+
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getY_Return_zeroAsValue() {
+        String given = "inputfield_0";
+
+        int actual = PlayerCharacter.getY();
 
         int expected = 0;
 
