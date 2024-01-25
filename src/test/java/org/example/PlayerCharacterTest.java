c93ec956ecbab8cabc2a9d065ec346b89428a9eb
R-Schreyer <roland_schreyer@web.de>
2024-01-25 11:22:16 +0100

    Initial commit

---
+++ src/test/java/org/example/PlayerCharacterTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerCharacterTest {
+
+    @Test
+    void getX_Return_zeroAsValue(){
+        int given;
+
+
+        int actual;
+
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+
+}
